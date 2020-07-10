e1fbaad429e25e9c27ab0c49cde9b949def44647
vardhanjai123 <shekharjai07@gmail.com>
2020-07-10 21:44:38 +0530

    Initial Project Setup.All the required dependecies which are required initially are added.Basic Project Template

---
+++ src/main/java/com/jaivardhan/springbootredditclone/SpringbootRedditCloneApplication.java
@@ -0,0 +1,13 @@
+package com.jaivardhan.springbootredditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedditCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaivardhan/springbootredditclone/SpringbootRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaivardhan.springbootredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
