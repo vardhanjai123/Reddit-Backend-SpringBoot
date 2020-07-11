fd1902eadad7177014070a45c36984113585c305
vardhanjai123 <shekharjai07@gmail.com>
2020-07-11 22:10:03 +0530

    Overrided the InitialWebSecurityConfiguration.Disabled the csrf and added paths which need to be seen to all and the remaining paths are made to authenticate

---
+++ src/main/java/com/jaivardhan/springbootredditclone/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.jaivardhan.springbootredditclone.config;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
